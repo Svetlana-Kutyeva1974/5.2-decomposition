78e22f955acc60f0fd4321cd7c135c6d8e6d72f9
Svetlana Kutyova <kutyovas@yandex.ru>
2022-05-05 20:56:39 +0300

    package-edit

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -2,7 +2,7 @@ import React from 'react';
 //import { nanoid } from 'nanoid';
 
 function Widget({widget}) {
-  console.log('массивы для виджетов', widget, widget.widgetName);
+  console.log('массивы для виджетов', widget, widget.widgetName, widget.widgetBody);
   console.log('keys array', Object.keys(widget.widgetBody), widget.widgetBody.length); 
   /*const item = widget.widgetBody.map(item => {
               console.log('ключи элемента', Object.keys(item));
@@ -20,7 +20,7 @@ function Widget({widget}) {
             <ul className="title" >{widget.widgetName}</ul>
            
             {widget.widgetBody.map(item => {
-              console.log('ключи элемента', Object.keys(item));
+              console.log('ключи элемента', Object.keys(item), item.id);
               const arrayKeys= Object.keys(item);
                 const newarr = arrayKeys.slice(1).map(itm => {
                     return (
